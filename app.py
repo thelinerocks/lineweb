4d39f090333e3fc661ba8330a128ccc865015f5f
Charith Amarasinghe <ca508@ic.ac.uk>
2018-01-27 20:03:13 +0000

    Add socket io streaming

---
+++ app.py
@@ -0,0 +1,43 @@
+import flask
+import flask_redis
+import flask_socketio
+import time
+import threading
+
+redis_store = flask_redis.FlaskRedis()
+socketio = flask_socketio.SocketIO()
+
+def get_data_for_hashtag(tag):
+    return redis_store.lrange(tag, 0, 1000)
+
+def broadcast_thread():
+    while True:
+        # sleeping for 50ms
+        time.sleep(0.2)
+        # get all keys for datapoints:
+        keys = redis_store.keys(pattern="points-*")
+        for k in keys:
+            socketio.emit('points', {"p": redis_store.lindex(k, 0)})
+
+#, namespace="/{}".format(k)
+def create_app():
+    app = flask.Flask(__name__)
+    redis_store.init_app(app)
+    socketio.init_app(app)
+
+    thread = threading.Thread(target=broadcast_thread)
+    thread.daemon = True
+    thread.start()
+    return app
+
+app = create_app()
+
+@app.route("/")
+def line():
+    tag1 = "#ichackupper"
+    tag2 = "#ichacklower"
+    tag_data = {
+        tag1 : get_data_for_hashtag(tag1),
+        tag2 : get_data_for_hashtag(tag2)
+    }
+    return flask.render_template("index.html", tag_data=tag_data)
---
+++ examples/provider.py
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+import redis
+import random
+import time
+import logging
+
+logger = logging.getLogger("provider")
+
+if __name__=="__main__":
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+            '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+    val = 0
+    hashtag1 = "points-#icupper"
+    hashtag2 = "points-#iclower"
+
+    while True:
+        time.sleep(0.05)
+        val = random.random()*1
+        r.lpush(hashtag1, val)
+        if r.llen(hashtag1) > 1000:
+            r.rpop(hashtag1)
+
+        val = random.random()*1
+        r.lpush(hashtag2, val)
+        if r.llen(hashtag2) > 1000:
+            r.rpop(hashtag2)
+        logger.debug("Sent datapoints - q length %d", r.llen(hashtag2))
